ca8dcce1cb57c85f9ea3c29db91ea81dc6f01574
Junshoong <junshoong@gmail.com>
2021-10-04 00:23:31 +0900

    update dotfiles

---
+++ README.md
@@ -0,0 +1,7 @@
+## Dotfiles
+
+Base OS : Archlinux
+
+## Installation
+
+run install.sh
---
+++ archlinux_install.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
